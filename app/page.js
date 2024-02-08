ea94dfdd1cc152fd5bff4ca374615ba41ede0bba
Harun <harunabdi8@gmail.com>
2024-02-08 21:05:57 +0100

    landing page work

--- app/page.js
+++ app/page.js
@@ -3,32 +3,98 @@ import {
   ResizablePanel,
   ResizablePanelGroup,
 } from "@/components/ui/resizable";
+import Image from "next/image";
+import LPBI from "@/public/landingPage.svg";
+import { Button } from "@/components/ui/button";
+import Typewriter from "@/public/typewriter.svg";
+import Upload from "@/public/upload.svg";
 
 export default function Home() {
   return (
-    <div>
-      <div className="flex justify-center mt-28	">
-        <ResizablePanelGroup
-          direction="vertical"
-          className="min-h-[200px] max-w-lg rounded-lg ">
-          <ResizablePanel defaultSize={25}>
-            <div className="flex h-full items-center justify-center p-6">
-              <p className=" dark:text-slate-300	 font-serif font-bold text-2xl">
-                {" "}
-                Accelerate your work with{" "}
-                <span className="text-blue-600		"> AskPDF</span>
-              </p>
-            </div>
-          </ResizablePanel>
-          <ResizableHandle />
-          <ResizablePanel defaultSize={75}>
-            <div className="flex h-full items-center justify-center p-6"></div>
-          </ResizablePanel>
-        </ResizablePanelGroup>
+    <>
+      <div class=" mt-16 flex justify-center grid grid-rows-3 grid-flow-col gap-4">
+        <div class="row-span-3 ...">
+          <Image
+            src={Typewriter}
+            width={650}
+            height={650}
+            alt="landing page main image"></Image>
+
+          <div className="flex justify-start mt-6 ">
+            <Button size="xlg" variant="homepage">
+              Try AskPDF for free
+            </Button>
+          </div>
+        </div>
+        <div class="col-span-2 ...">
+          <p className=" dark:text-slate-300 font-serif font-bold text-6xl leading-tight">
+            <span className="ml-14"> the handiest </span> <br />
+            way to organize <br />
+            <span className="text-blue-600		"> reading</span>
+          </p>
+        </div>
+        <div class="row-span-2 col-span-2 mx-auto">
+          <Image
+            src={LPBI}
+            width={350}
+            height={350}
+            alt="landing page main image"></Image>
+        </div>
+        <div></div>
       </div>
-      <section className="flex justify-center">
-        <h1>hi</h1>
+      <h1>hi</h1>
+
+      <section className=" bg-blue-950	">
+        <div className=" flex justify-center">
+          <p className=" mx-auto mt-8 text-zinc-100 dark:text-slate-300 font-serif font-bold text-3xl leading-tight">
+            How does it work
+          </p>
+        </div>
+        <div className=" mt-12 pb-8 flex justify-center gap-16">
+          <div>
+            <Image
+              className="mx-auto"
+              src={Upload}
+              width={50}
+              height={50}
+              alt="landing page main image"></Image>
+            <p className=" flex justify-center mt-8 text-zinc-100 dark:text-slate-300 font-serif font-bold text-lg leading-tight">
+              Upload your PDF file
+            </p>
+          </div>
+          <div>
+            <Image
+              className="mx-auto"
+              src={Upload}
+              width={50}
+              height={50}
+              alt="landing page main image"></Image>
+            <p className=" flex justify-center mt-8 text-zinc-100 dark:text-slate-300 font-serif font-bold text-lg leading-tight">
+              Ask any question about your PDF
+            </p>
+          </div>
+          <div>
+            <Image
+              className="mx-auto"
+              src={Upload}
+              width={50}
+              height={50}
+              alt="landing page main image"></Image>
+            <p className=" flex justify-center mt-8 text-zinc-100 dark:text-slate-300 font-serif font-bold text-lg leading-tight">
+              Let AskPDF find the answer
+            </p>
+          </div>
+        </div>
       </section>
-    </div>
+
+      <section className=" ">
+        <div className=" flex justify-center">
+          <p className=" mt-8 text-gray-900 dark:text-slate-300 font-serif font-bold text-3xl leading-tight">
+            Loved by many
+          </p>
+        </div>
+        <div className="mt-12 flex justify-center gap-16"></div>
+      </section>
+    </>
   );
 }
--- components/footer.jsx
+++ components/footer.jsx
@@ -3,12 +3,17 @@ const Footer = () => {
   return (
     <footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
       <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+        <span className="text-orange-500 text-4xl text-bold flex justify-center	">
+          {" "}
+          BETA
+        </span>{" "}
+        Version: {version}
+        {/* <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2023{" "}
           <a href="https://AskPDF.com/" class="hover:underline mr-5">
-            AskPDF {" "}
+            AskPDF{" "}
           </a>
-          Version: {version}
+          <span className="text-orange-500	"> BETA</span> Version: {version}
         </span>
         <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
           <li>
@@ -31,7 +36,7 @@ const Footer = () => {
               Contact
             </a>
           </li>
-        </ul>
+        </ul> */}
       </div>
     </footer>
   );
