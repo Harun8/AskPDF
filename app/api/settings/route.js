37b0d269a8e4f6ed4329e67bdfbd66b691e5ba05
Harun <harunabdi8@gmail.com>
2024-02-12 19:47:26 +0100

    headache

--- app/api/settings/route.js
+++ app/api/settings/route.js
@@ -1,36 +1,58 @@
 import { SITE_URL } from "@/util/endpoints";
 import { stripe } from "@/util/stripe/stripe";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-const { createClient } = require("@supabase/supabase-js");
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-);
+import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import SupabaseClient from "@supabase/auth-helpers-nextjs";
+
 export default async function handler(req, res) {
-  console.log("SETTINGS BTN ROUTE I AM HERE");
-  const supabaseServerClient = createServerComponentClient({
-    req,
-    res,
-  });
+  // Initialize server-side Supabase client
+  const supabaseServerClient = createServerSupabaseClient({ req, res });
+  // Retrieve the current user session from the server-side client
+
+  //   const { user } = await supabase.auth.api.getUserByCookie(req);
 
   const {
     data: { user },
-  } = await supabase.auth.getUser();
+  } = await supabaseServerClient.auth.getUser();
 
-  if (!user) res.status(401).send("not logged in");
+  if (!user) {
+  }
+  console.log("data", user);
 
-  const { data: profile } = await supabase
+  // Proceed with your logic now that you have the user
+  const { data: profile, error } = await supabase
     .from("profile")
     .select("stripe_customer_id")
     .eq("userid", user.id)
     .single();
 
+  if (error) {
+    return new Response(
+      JSON.stringify({ msg: "retrieval of profile row failed.." }),
+      {
+        status: 404, // Set the status code to 200 (OK)
+        headers: {
+          "Content-Type": "application/json", // Set the Content-Type header to 'application/json'
+        },
+      }
+    );
+  }
+
+  console.log("data from profile table", profile);
+
+  // Your existing Stripe session creation logic...
   const session = await stripe.billingPortal.sessions.create({
     customer: profile.stripe_customer_id,
-    return_url: "/",
+    return_url: "http://localhost:3000",
   });
 
-  res.send({ url: session.return_url });
+  console.log("session", session);
+  return new Response(JSON.stringify({ url: session.url }), {
+    status: 200, // Set the status code to 200 (OK)
+    headers: {
+      "Content-Type": "application/json", // Set the Content-Type header to 'application/json'
+    },
+  });
 }
 
 export { handler as GET };
