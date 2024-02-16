5e61aa883792330a211b9fb001d8d062afb5291f
Harun <harunabdi8@gmail.com>
2024-02-16 14:55:19 +0100

    wow

--- components/ConversationDisplay.jsx
+++ components/ConversationDisplay.jsx
@@ -6,12 +6,12 @@ const ConversationDisplay = ({ conversation }) => {
         {/* Adjust the height as needed */}
         <div className=" ">
           <div
-            className={` border-4 rounded-lg shadow-xl h-[500px]  dark:bg-gray-900					`}>
+            className={` overflow-y-auto border-4 rounded-lg shadow-xl h-[500px]  dark:bg-gray-900					`}>
             {conversation.map((msg, index) => {
               let textColor =
                 msg.type === "user"
-                  ? "text-blue-500 bg-zinc-200 dark:bg-gray-800 "
-                  : "text-green-500 bg-zinc-200 dark:bg-gray-800 ";
+                  ? "text-zinc-800	 bg-zinc-200 dark:bg-gray-800 "
+                  : "text-blue-500 font-semibold	 bg-zinc-200 dark:bg-gray-800 ";
               return (
                 <div
                   key={index}
