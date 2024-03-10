a84269777a9c9f44112182d6d397b70e6d72dffb
Harun <harunabdi8@gmail.com>
2024-03-10 19:30:21 +0100

    yo

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,16 @@
-"use client";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Nav from "@/components/Nav";
 // import { cookies } from "next/headers";
 import { Inter as FontSans } from "next/font/google";
-import React, { useEffect, useState } from "react";
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-import { Analytics } from "@vercel/analytics/react";
+// import React, { useEffect, useState } from "react";
+// import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+// import { Analytics } from "@vercel/analytics/react";
 import { supabase } from "@/lib/supabase";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+// import { SpeedInsights } from "@vercel/speed-insights/next";
 // import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import Footer from "@/components/footer";
+// import { createServerComponentClient } from "@/lib/supabaseServer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,17 +20,18 @@ export const fontSans = FontSans({
 });
 
 export default function RootLayout({ children }) {
-  const [session, setSession] = useState(null);
+  // const [session, setSession] = useState(null);
 
-  useEffect(() => {
-    // Fetch session on component mount
-    setSession(supabase.auth.getSession());
+  // useEffect(() => {
+  //   // Fetch session on component mount
+  //   setSession(supabase.auth.getSession());
 
-    // Set up a session state listener for real-time updates
-    supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session);
-    });
-  }, []);
+  //   // Set up a session state listener for real-time updates
+  //   supabase.auth.onAuthStateChange((_event, session) => {
+  //     setSession(session);
+  //   });
+  // }, []);
+
   // const cookieStore = cookies();
 
   // const supabase = createServerComponentClient({ cookies: () => cookieStore });
@@ -41,7 +42,7 @@ export default function RootLayout({ children }) {
   return (
     <html class="" lang="en">
       <body class="flex flex-col min-h-screen w-full bg-zinc-100	 dark:bg-gray-800">
-        <Nav session={session}></Nav>
+        <Nav /*session={session} */></Nav>
         <main class="flex-grow">
           {children}
           {/* <Analytics></Analytics>
