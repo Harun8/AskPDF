d7f13e5209593dc3ea2a1f9f74590c099fc9f096
Harun <harunabdi8@gmail.com>
2024-03-25 16:19:34 +0100

    darkmode fix

--- components/footer.jsx
+++ components/footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   let version = packageInfo.version;
 
   return (
-    <section class="bg-gray-800">
+    <section class="bg-zinc-100 dark:bg-gray-800">
       <div class="max-w-screen-xl px-4 py-12 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8">
         <nav class="flex flex-wrap justify-center -mx-5 -my-2">
           {/* <div class="px-5 py-2">
