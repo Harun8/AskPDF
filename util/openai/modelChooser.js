a6bf134f7734b00eb4985b9eefbe02dd7d1f03ba
Harun8 <harunabdi8@gmail.com>
2025-04-16 11:09:10 +0200

    model update

--- util/openai/modelChooser.js
+++ util/openai/modelChooser.js
@@ -3,19 +3,19 @@ function modelChooser(plan) {
   // change to a switch case when u see this
   if (plan == null) {
     // free plan
-    modelChooser = "gpt-4-0125-preview"; // 1 pdf upload 
+    modelChooser = "gpt-4.1-nano"; // 1 pdf upload 
   } else if (
     plan === "price_1OpYyoBzVPtG7eO2xJAtCiFa" ||
     plan === "price_1OpYlBBzVPtG7eO2D4il1zcz"
   ) {
     // premium
-    modelChooser = "gpt-4-0125-preview"; // 50 pdf uploads
+    modelChooser = "gpt-4.1-nano"; // 50 pdf uploads
   } else if (
     plan === "price_1OpYzEBzVPtG7eO2xS53tGQ0" ||
     plan === "price_1OpYzuBzVPtG7eO2oCFa8Sc7"
   ) {
     // ultimate plan
-    modelChooser = "gpt-4o-mini"; // 100 pdf uploads
+    modelChooser = "gpt-4.1-nano"; // 100 pdf uploads
   }
 
   return modelChooser;
